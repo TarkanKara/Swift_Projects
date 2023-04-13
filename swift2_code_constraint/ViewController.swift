dab3d4c31f5edaaab1d4bae82bb08568489b3511
Tarkan Kara <unnahmet23@hotmail.com>
2023-04-13 16:21:45 +0300

    Initial Commit

---
+++ swift2_code_constraint/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  swift2_code_constraint
+//
+//  Created by Tarkan Kara on 13.04.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
