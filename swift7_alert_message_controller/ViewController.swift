6c95d83324fdd1a2bb2d3f941e7df98f15b0dddf
Tarkan Kara <unnahmet23@hotmail.com>
2023-04-13 23:42:49 +0300

    Initial Commit

---
+++ swift7_alert_message_controller/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  swift7_alert_message_controller
+//
+//  Created by Tarkan Kara on 13.04.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
