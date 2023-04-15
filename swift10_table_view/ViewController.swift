32c6d7a6bd756997ae1109db510b07718f6545c4
Tarkan Kara <unnahmet23@hotmail.com>
2023-04-15 19:29:57 +0300

    tableView add UITableViewDelegate and UITableViewDataSource

--- swift10_table_view/ViewController.swift
+++ swift10_table_view/ViewController.swift
@@ -7,13 +7,34 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+// UITableViewDelegate ekliyoruz
+// UITableViewDataSource
+// UITableViewDelegate table view’a ait görüntünün ve kullanıcı etkileşimlerini yönetir. Örnek vermek gerekirse table view üzerinde bir cell seçildiğinde ya da sağa & sola kaydırıldığında bu delegate metodları table view’ın bilgilendirilmesine yardımcı olur.
+// UITableViewDataSource table view üzerinde görüntülenecek verinin bölümlerinden (section) ve bu bölümlerin satır sayısına (row) gerekli düzenlemeleri yapan metodları içerir.
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        // ViewControllarımıza tableView için delegate ve dataSource bildiriyoruz
+        tableView.delegate = self
+        tableView.dataSource = self
+        
     }
 
-
+    // numberOfRowsInSection ----> kaç tane row olacak
+    // cellForRow atIndexPath ----> hücrenin içerisinde neler gösterilecek
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    // Table View Cell, Table View’a ait tekrar eden satırlardur. Table View Cell ise UITableViewCell sınıfının bir oluşumudur.
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.textLabel?.text = "Tarkan"
+        return cell
+    }
 }
 
---
+++ swift10_table_view/ViewController2.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController2.swift
+//  swift10_table_view
+//
+//  Created by Tarkan Kara on 15.04.2023.
+//
+
+import UIKit
+
+class ViewController2: UIViewController {
+
+    @IBOutlet weak var labelText: UILabel!
+    @IBOutlet weak var imageVieww: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+
+    
+
+}
