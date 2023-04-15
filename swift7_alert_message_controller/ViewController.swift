a2d5dbd714394cd7c459cf98745e0c1e40697ea8
Tarkan Kara <unnahmet23@hotmail.com>
2023-04-15 10:11:27 +0300

    alert_message_controller

--- swift7_alert_message_controller/ViewController.swift
+++ swift7_alert_message_controller/ViewController.swift
@@ -9,11 +9,44 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var password2TextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
 
+    @IBAction func signUpBtn(_ sender: Any) {
+        
+        if emailTextField.text == "" {
+            //Email Boş girilemez
+            self.customAlertDialog(title: "EMAİL HATA!", message: "Email Girirniz!")
+        } else if passwordTextField.text == "" {
+            //password alanı boş girilemez
+            self.customAlertDialog(title: "PASSWORD HATA", message: "Parola Giriniz!")
+        } else if passwordTextField.text != password2TextField.text {
+            // parolalar uyuşmuyor
+            self.customAlertDialog(title: "PASSWORD HATA", message: "Parolar Uyuşmuyor")
+        } else {
+            //Giriş Başaralı
+            self.customAlertDialog(title: "SUCCESS", message: "Giriş Başarılı")
+        }
+        
+    }
+    
+    func customAlertDialog(title : String, message : String){
+        let alertBtn = UIAlertController(title: title, message: message, preferredStyle: UIAlertController.Style.alert)
+        
+        let okButton = UIAlertAction(title: "OK", style: UIAlertAction.Style.default) {
+            (UIAlertAction) in
+            //OK button'una tiklanınca olacaklar.
+            print("ok button tıklandı")
+        }
+        alertBtn.addAction(okButton)
+        self.present(alertBtn, animated: true, completion: nil)
+    }
 }
 
