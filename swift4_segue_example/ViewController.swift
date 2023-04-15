6db4ce1b6e26ed3ab2663a6cf6412c186e332742
Tarkan Kara <unnahmet23@hotmail.com>
2023-04-15 09:42:21 +0300

    Initial Commit

---
+++ swift4_segue_example/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  swift4_segue_example
+//
+//  Created by Tarkan Kara on 15.04.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
